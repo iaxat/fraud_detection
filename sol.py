5c0fbd67627743b84bdf65ffb463a0998da58b16
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-17 18:02:41 -0500

    up

--- sol.py
+++ sol.py
@@ -5,6 +5,7 @@ def data_collector():
     input_data = ['AB5', 'BC4', 'CD8', 'DC8', 'DE6', 'AD5', 'CE2', 'EB3', 'AE7']
     graph_dict = {'A':[],'B':[],'C':[],'D':[],'E':[]}
     temp_cal = 0
+    stops = 0
 
     for i in input_data:
         temp_str = i[1]+i[2]
@@ -13,10 +14,25 @@ def data_collector():
 
     print(graph_dict)
     # soltiuon for question
-    # 1 to 5
-    find_results = ['ABC']
-    
+    # 1 ABC
+    if 'A' in graph_dict.keys():
+        for k in graph_dict['A']:
+            
+            if 'B' in k:
+                list(k)
+                temp_cal += int(k[1])
 
+                if 'B' in graph_dict.keys():
+                    for k in graph_dict['B']:
+                        
+                        if 'C' in k:
+                            list(k)
+                            temp_cal += int(k[1])
+
+    print('Output 1. A-B-C',temp_cal)
+
+    
+            
 
 
 data_collector()
