64da177086c5cc73ba9f741cb6e4b27b39fc0ee0
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-17 15:42:23 -0500

    up

--- demo.py
+++ demo.py
@@ -1,24 +1,22 @@
 
-def pathfinder():
+
+# testing of input data
+# testing of data arrangement
+
+
+# function to define nodes
+def path_finder():
+    # node_req = input('A B C D E are available')
+    graph_dict = {}
+    input_data = ['AB5', 'BC4', 'CD8', 'DC8', 'DE6', 'AD5', 'CE2', 'EB3', 'AE7']
     
-
-
-
-
-
-def test_cases_creator():
-
-    # creating test cases
-    # using user input
-
-    nodes_input = input('Number of Nodes Required')
-    
-
-
-
-
-
-    pathfinder()
-
+    # creating path in dict
+    # picking up the 1st letter for each array element
+    for i in input_data:
+        node_letter = i[0]
+        temp_str = i.split()
+        temp_str = temp_str.pop(0)
+        if node_letter not in graph_dict:
+            graph_dict[node_letter] = temp_str
 
 # https://www.geeksforgeeks.org/generate-graph-using-dictionary-python/
